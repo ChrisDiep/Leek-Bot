3ef836a2e859cb23f715dfcf4ca2e621715c5168
Chris <chrisldiep@gmail.com>
2020-11-26 14:49:44 -0800

    Created skeleton file system and added profile_requests for requesting league profiles

---
+++ Requests/League/profiles.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import requests
+
+
+class profile_requests:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.headers = {'X-Riot-Token': self.api_key}
+        self.url = "https://na1.api.riotgames.com"
+
+    def get_summonerid(self, summoner_name):
+        url = f'{self.url}/lol/summoner/v4/summoners/by-name/{summoner_name}'
+        resp = requests.get(url, headers=self.headers)
+        return resp.json()
+
+    def get_ranked_stats(self, summoner_id):
+      url = f'{self.url}/lol/league/v4/entries/by-summoner/{summoner_id}'
+      resp = requests.get(url, headers=self.headers)
+      return resp.json()
+
+    def get_league_info(self, summoner_name):
+      profile_info = self.get_summonerid(summoner_name)
+      league_info = self.get_ranked_stats(profile_info['id'])
+      return [profile_info, league_info]
