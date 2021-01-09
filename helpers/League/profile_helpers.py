4db6750d8cdb621d2610f50afda5bf5ea8c05e4e
Chris <chrisldiep@gmail.com>
2021-01-08 20:50:26 -0800

    Added docstrings

--- helpers/League/profile_helpers.py
+++ helpers/League/profile_helpers.py
@@ -2,5 +2,6 @@ import re
 
 
 def clean_input(words):
+    """ Removes non-alphanumeric values and joins the input together """
     parsed = ''.join(map(lambda word: re.sub(r'[^a-zA-Z0-9]', '', word), words))
     return parsed
