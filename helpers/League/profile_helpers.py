c51aa754e237011a2efb66f232ae105f8cdb7cf0
Chris <chrisldiep@gmail.com>
2021-01-09 00:03:34 -0800

    Added functions to get champion ids/names and version of ddragon

--- helpers/League/profile_helpers.py
+++ helpers/League/profile_helpers.py
@@ -1,7 +1,23 @@
 import re
+import requests
 
 
 def clean_input(words):
     """ Removes non-alphanumeric values and joins the input together """
     parsed = ''.join(map(lambda word: re.sub(r'[^a-zA-Z0-9]', '', word), words))
     return parsed
+
+def get_champions(version):
+    url = f'http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json'
+    resp = requests.get(url)
+    champions_obj = resp.json()["data"]
+    champion_ids = {}
+    for champion in champions_obj:
+        print(champion)
+        champion_ids[champions_obj[champion]["key"]] = champion
+    print(champion_ids)
+    return champion_ids
+
+def get_version():
+    resp = requests.get('https://ddragon.leagueoflegends.com/api/versions.json')
+    return resp.json()[0]
